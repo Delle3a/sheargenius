85023118ef107b2e9ef02e62311244fb6c7010b2
Delle3a <chebbiadam12@gmail.com>
2025-08-11 14:33:15 +0100

    3asba

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAYt9h2tnERoXCeic3y2rWBvGpta3zErmM",
-  authDomain: "barbershop-85c50.firebaseapp.com",
-  projectId: "barbershop-85c50",
-  storageBucket: "barbershop-85c50.firebasestorage.app",
-  messagingSenderId: "475868017335",
-  appId: "1:475868017335:web:2302ae04e26e910bfa9332",
-  measurementId: "G-VG5FMVK4CH"
+  apiKey: "AIzaSyDxjH-4J2qZ_ePnYs1CIWOJZ0we6FODM0k",
+  authDomain: "shear-geniu.firebaseapp.com",
+  projectId: "shear-geniu",
+  storageBucket: "shear-geniu.firebasestorage.app",
+  messagingSenderId: "215757153302",
+  appId: "1:215757153302:web:0516d63981d2d471cdc137"
 };
 
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+export { db, app };
+
+    
